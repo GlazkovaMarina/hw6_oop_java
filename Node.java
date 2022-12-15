d450879a1a86a60ba331cc044276299d947f7be5
Glazkova Marina <glazkova.mv@bk.ru>
2022-12-15 11:24:22 +0300

    add task 1

---
+++ Node.java
@@ -0,0 +1,19 @@
+public class Node {
+    Node next;
+    User user;
+    public Node(User user) {
+        this.next = null;
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+    
+    public Node getNext() {
+        return next;
+    }
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
---
+++ Persister.java
@@ -0,0 +1,5 @@
+public interface Persister{
+	public void saveToTxt(String fileName, User user);
+	public void saveLinkedList(User user);
+	public void reportToPrinter(User user);
+}
